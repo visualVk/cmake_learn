74797cb33c528f6c3dda911e1acebe5a3d54e916
Tom <2048137831@qq.com>
2021-08-25 17:30:45 +0800

    01-base C-hello

---
+++ 01-base/C-hello/build/CMakeFiles/hello_binary.dir/src/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/hello_binary.dir/src/main.cpp.o: \
+ /home/visualvk/code/cpp/cmake/01-base/C-hello/src/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/visualvk/code/cpp/cmake/01-base/C-hello/include/static/Hello.h
